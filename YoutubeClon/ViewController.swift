d48554d6307a9e92ec14fe04305a1393c7d06b75
ahdadzia <ahda.fakhri@gmail.com>
2021-03-15 10:30:48 +0700

    Initial Commit

---
+++ YoutubeClon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeClon
+//
+//  Created by Ahda  Dzia Ulhaq on 15/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
